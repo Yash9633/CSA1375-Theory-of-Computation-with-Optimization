79a1d06c57387ba122e7b81503d65abf8e2187a3
Yash9633 <113875862+Yash9633@users.noreply.github.com>
2023-05-15 14:38:17 +0530

    Create Program Ep35 DFA String starts with 1 ends with 0.c

---
+++ Program Ep35 DFA String starts with 1 ends with 0.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+main(){
+	int max,i;
+	printf("Length: ");
+	scanf("%d",&max);
+	int str[max];
+	for(i=0;i<max;i++){
+		scanf("%d",&str[i]);
+	}
+	for(i=0;i<max;i++){
+		printf("%d ",str[i]);
+	}
+	printf("\nInitial Input is %d",str[0]);
+	printf("\nFinal Input is %d",str[max-1]);
+	if(str[0]==1){
+		if(str[max-1]==0){
+			printf("\n\n-----STRING ACCEPTED!!-----");
+		}
+		else{
+			printf("\n\n-----INVALID STRING...-----");
+		}
+	}
+	else{
+		printf("\n\n-----INVALID STRING...-----");
+	}
+}
